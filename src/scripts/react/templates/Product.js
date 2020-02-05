67e54f20f79900fc15fdc2414021cfaed5356c06
James Lacandula <jlacandula@gmail.com>
2020-02-06 02:21:44 +0800

    Update Product.js

--- src/scripts/react/templates/Product.js
+++ src/scripts/react/templates/Product.js
@@ -70,26 +70,18 @@ const Product = (props) => {
   // },[data, loading]);
 
   useEffect(() => {
-    fetch("/products/black-leather-bag.js")
+    fetch("/products/black-leather-baeg.js")
       .then(res => res.json())
       .then(
         (result) => {
           console.log('fetched data:');
           console.log(result);
-          // this.setState({
-          //   isLoaded: true,
-          //   items: result.items
-          // });
         },
         // Note: it's important to handle errors here
         // instead of a catch() block so that we don't swallow
         // exceptions from actual bugs in components.
         (error) => {
           console.log('ERROR fetching data:');
-          // this.setState({
-          //   isLoaded: true,
-          //   error
-          // });
         }
       )
   },[]);
