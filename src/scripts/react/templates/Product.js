06cb8cf80ea43a9431bab756c25ff28f039b71db
James Lacandula <jlacandula@gmail.com>
2020-02-06 02:20:32 +0800

    working fetch api GET on ajax cart api

--- src/scripts/react/templates/Product.js
+++ src/scripts/react/templates/Product.js
@@ -60,14 +60,39 @@ const Product = (props) => {
 
   const { data, loading, error } = useQuery(createProductQuery(props.match.params.handle));
   
+  // useEffect(() => {
+  //   if (data) {
+  //     setProductData(data.shop.productByHandle);
+  //   }
+  //   if (!loading) {
+  //     setLoaded(true);
+  //   }
+  // },[data, loading]);
+
   useEffect(() => {
-    if (data) {
-      setProductData(data.shop.productByHandle);
-    }
-    if (!loading) {
-      setLoaded(true);
-    }
-  },[data, loading]);
+    fetch("/products/black-leather-bag.js")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log('fetched data:');
+          console.log(result);
+          // this.setState({
+          //   isLoaded: true,
+          //   items: result.items
+          // });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          console.log('ERROR fetching data:');
+          // this.setState({
+          //   isLoaded: true,
+          //   error
+          // });
+        }
+      )
+  },[]);
 
   if (error) return <p>There is an error!</p>;
   
